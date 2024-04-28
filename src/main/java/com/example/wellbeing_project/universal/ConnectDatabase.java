3fcb02f8225d5193e0103882503e89b5c7e8f5e2
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-04-28 20:27:32 +1000

    Add files via upload

---
+++ src/main/java/com/example/wellbeing_project/universal/ConnectDatabase.java
@@ -0,0 +1,22 @@
+package com.example.signup01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDatabase {
+    // Create variable that holds URL to SQLite database
+    private static final String URL = "jdbc:sqlite:../Wellness_Tracker.db/";
+
+    // Create method to connect application to database
+    public static Connection connect() {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connection successful");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed - Reason: " + e.getMessage());
+            return null;
+        }
+    }
+}
