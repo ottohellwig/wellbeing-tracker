21f6cd37cd84cd1cb5659294fd249b995a29484d
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-04-30 03:21:51 +1000

    Add files via upload

---
+++ src/main/java/com/example/wellbeing_project/universal/ConnectDatabase.java
@@ -0,0 +1,22 @@
+package com.example.wellbeing_project.universal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDatabase {
+    // Create variable that holds URL to SQLite database
+    private static final String URL = "jdbc:sqlite:/C:/Users/USER/IdeaProjects/cab302-wellbeing-project2/Wellness_Tracker.db/";
+
+    // Create method to connect application to database
+    public static Connection connect() {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connection successful");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed - Reason: " + e.getMessage()); // See errors
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/example/wellbeing_project/universal/ErrorAlert.java
@@ -0,0 +1,22 @@
+package com.example.wellbeing_project.universal;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+// Create a class to display errors in application
+public class ErrorAlert {
+
+    public static void displayError(String errorTitle, String message) {
+        // Create a new Alert
+        Alert alert = new Alert(AlertType.ERROR);
+
+        // Set the title of the alert
+        alert.setTitle(errorTitle);
+
+        // Set the message
+        alert.setContentText(message);
+
+        // Show the alert
+        alert.showAndWait();
+    }
+}
