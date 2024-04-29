4539e8e3f0c211d2ec92aa71c2af054b159212cf
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-04-30 03:09:35 +1000

    Delete src/main/java/com/example/wellbeing_project/signup directory

--- src/main/java/com/example/wellbeing_project/signup/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.signup01 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.signup01 to javafx.fxml;
-    exports com.example.signup01;
-}
