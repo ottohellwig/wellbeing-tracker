08f35308bb9fa831ee4dfeb41a64a303799de7c8
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-04-30 03:20:33 +1000

    Add files via upload

---
+++ src/main/java/com/example/wellbeing_project/resources_page/ResourcesApplication.java
@@ -0,0 +1,37 @@
+package com.example.wellbeing_project.resources_page;
+
+import com.example.wellbeing_project.login.LoginApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+// Constructor method
+public class ResourcesApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Get fxml document
+        Parent root = FXMLLoader.load(getClass().getResource("/resources_page-view.fxml"));
+        stage.setTitle("Wellbeing Tracker");
+
+        // Create scene
+        Scene scene = new Scene(root, 800, 500);
+
+        // CSS stylesheet
+        String stylesheet = com.example.wellbeing_project.signup.SignupApplication.class.getResource("/stylesheet2.css").toExternalForm();
+
+        // Apply CSS Stylesheet
+        scene.getStylesheets().add(stylesheet);
+
+        // Set scene to the stage
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
