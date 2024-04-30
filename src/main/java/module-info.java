524d94c44c2a96b2dba39621e88997507c9f9036
Otto Hellwig <ottohellwig@hotmail.com>
2024-04-30 16:38:07 +1000

    Update module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,13 @@ module com.example.wellbeing_project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
     requires org.kordamp.bootstrapfx.core;
 
+    opens com.example.wellbeing_project.signup to javafx.fxml;
+    opens com.example.wellbeing_project.universal to javafx.fxml;
     opens com.example.wellbeing_project to javafx.fxml;
+
     exports com.example.wellbeing_project;
+    exports com.example.wellbeing_project.signup;
+    exports com.example.wellbeing_project.universal;
 }
