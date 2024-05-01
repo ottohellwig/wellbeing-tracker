0ab875c3e3185467622ddfdf214d183e2b1d21e7
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-05-01 18:56:21 +1000

    Update HomeApplication.java

--- src/main/java/com/example/wellbeing_project/HomeApplication.java
+++ src/main/java/com/example/wellbeing_project/HomeApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.example.wellbeing_project.login.LoginApplication;
 
 import java.io.IOException;
 
