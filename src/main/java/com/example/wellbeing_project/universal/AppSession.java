8cfbd08d0525180616fc2670fa61536ea4f27bce
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-05-14 23:07:38 +1000

    Update AppSession.java

--- src/main/java/com/example/wellbeing_project/universal/AppSession.java
+++ src/main/java/com/example/wellbeing_project/universal/AppSession.java
@@ -1,15 +1,15 @@
 package com.example.wellbeing_project.universal;
 
 public class AppSession {
-    // Static variable to store the logged-in user's ID
+    // Variable to store logged in user's ID
     public static int loggedInUserId;
 
-    // Getter method for the logged-in user's ID
+    // Getter method for the logged in user's ID
     public static int getLoggedInUserId() {
         return loggedInUserId;
     }
 
-    // Setter method for the logged-in user's ID
+    // Setter method for the logged in user's ID
     public static void setLoggedInUserId(int userId) {
         loggedInUserId = userId;
     }
