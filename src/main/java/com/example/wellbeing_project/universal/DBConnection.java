70353a17c85e0d0ea732e41801fcb9ddd534d4b2
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-05-14 23:30:28 +1000

    Update DBConnection.java
    added comments

--- src/main/java/com/example/wellbeing_project/universal/DBConnection.java
+++ src/main/java/com/example/wellbeing_project/universal/DBConnection.java
@@ -5,10 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
 
+// Class to connect application to database
 public class DBConnection {
     private static final String URL = "jdbc:sqlite:Wellness_Tracker.db/";
     private static Connection connection;
 
+    // Get instance to use
     public static Connection getInstance() {
         if (connection == null) {
             try {
