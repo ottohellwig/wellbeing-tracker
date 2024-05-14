5b596c9a65e6a559dbf7d6c9e0916cfbc99cb51c
Trent057 <162427327+Trent057@users.noreply.github.com>
2024-05-14 17:37:12 +1000

    Update module-info.java
    include new pages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.wellbeing_project {
     requires javafx.fxml;
     requires java.sql;
     requires org.kordamp.bootstrapfx.core;
+    requires java.prefs;
+    requires jbcrypt;
 
     opens com.example.wellbeing_project.signup to javafx.fxml;
     opens com.example.wellbeing_project.universal to javafx.fxml;
@@ -10,6 +12,7 @@ module com.example.wellbeing_project {
     opens com.example.wellbeing_project.login to javafx.fxml;
     opens com.example.wellbeing_project.resources_page to javafx.fxml;
     opens com.example.wellbeing_project.logout to javafx.fxml;
+    opens com.example.wellbeing_project.profile to javafx.fxml;
 
     exports com.example.wellbeing_project;
     exports com.example.wellbeing_project.signup;
@@ -17,4 +20,5 @@ module com.example.wellbeing_project {
     exports com.example.wellbeing_project.login;
     exports com.example.wellbeing_project.resources_page;
     exports com.example.wellbeing_project.logout;
+    exports com.example.wellbeing_project.profile;
 }
