854d1ea7a93566ef1273d4d49f5b1d674a6a6d5a
angerpac <anger.pac@connect.qut.edu.au>
2024-05-27 18:42:55 +1000

    added reports dao

--- src/main/java/com/example/wellbeing_project/ReportsController.java
+++ src/main/java/com/example/wellbeing_project/ReportsController.java
@@ -8,10 +8,8 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import dao.ReportDAO;
 
 public class ReportsController {
     @FXML
@@ -30,6 +28,8 @@ public class ReportsController {
 
     private boolean isWeeklyReport = true;
 
+    private final ReportDAO reportDAO = new ReportDAO();
+
     public void initialize() {
         showWeeklyReport();
         applicationColumn.setCellValueFactory(new PropertyValueFactory<>("application"));
@@ -39,106 +39,27 @@ public class ReportsController {
     @FXML
     private void showDailyReport() {
         isWeeklyReport = false;
-        XYChart.Series<String, Number> dailySeries = new XYChart.Series<>();
 
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:Wellness_Tracker.db");
-             Statement stmt = conn.createStatement()) {
-
-            String query = "SELECT " +
-            "CASE " +
-            "WHEN strftime('%H', StartTime) BETWEEN '00' AND '00' THEN '12am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '01' AND '01' THEN '1am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '02' AND '02' THEN '2am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '03' AND '03' THEN '3am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '04' AND '04' THEN '4am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '05' AND '05' THEN '5am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '06' AND '06' THEN '6am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '07' AND '07' THEN '7am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '08' AND '08' THEN '8am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '09' AND '09' THEN '9am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '10' AND '10' THEN '10am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '11' AND '11' THEN '11am' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '12' AND '12' THEN '12pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '13' AND '13' THEN '1pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '14' AND '14' THEN '2pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '15' AND '15' THEN '3pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '16' AND '16' THEN '4pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '17' AND '17' THEN '5pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '18' AND '18' THEN '6pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '19' AND '19' THEN '7pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '20' AND '20' THEN '8pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '21' AND '21' THEN '9pm' " +
-            "WHEN strftime('%H', StartTime) BETWEEN '22' AND '22' THEN '10pm' " +
-            "ELSE '11pm' " +
-            "END AS Hour, " +
-            "SUM(strftime('%s', EndTime) - strftime('%s', StartTime)) / 60 AS Duration " +
-            "FROM " +
-            "Usage " +
-            "WHERE " +
-            "UserId = 1 " +
-            "AND DATE(StartTime) = DATE('now') " +
-            "GROUP BY " +
-            "Hour " +
-            "ORDER BY " +
-            "strftime('%H', StartTime)";
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                String hour = rs.getString("Hour");
-                double timeInHours = rs.getInt("Duration") / 60.0;
-                dailySeries.getData().add(new XYChart.Data<>(hour, timeInHours));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        updateGraph(dailySeries, true);
+        updateGraph(reportDAO.getDailyReport(), true);
         detailedActivityTable.setVisible(false);
     }
 
     @FXML
     private void showWeeklyReport() {
         isWeeklyReport = true;
-       XYChart.Series<String, Number> weeklySeries = new XYChart.Series<>();
 
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:Wellness_Tracker.db");
-             Statement stmt = conn.createStatement()) {
-
-            String query = "SELECT " +
-                    "CASE " +
-                    "WHEN strftime('%w', StartTime) = '0' THEN 'Sunday' " +
-                    "WHEN strftime('%w', StartTime) = '1' THEN 'Monday' " +
-                    "WHEN strftime('%w', StartTime) = '2' THEN 'Tuesday' " +
-                    "WHEN strftime('%w', StartTime) = '3' THEN 'Wednesday' " +
-                    "WHEN strftime('%w', StartTime) = '4' THEN 'Thursday' " +
-                    "WHEN strftime('%w', StartTime) = '5' THEN 'Friday' " +
-                    "WHEN strftime('%w', StartTime) = '6' THEN 'Saturday' " +
-                    "END AS Day, " +
-                    "SUM(strftime('%s', EndTime) - strftime('%s', StartTime)) / 60 AS Duration " +
-                    "FROM Usage " +
-                    "WHERE UserId = 1 " +
-                    "AND DATE(StartTime) BETWEEN DATE('now', '-7 days') AND DATE('now') " +
-                    "GROUP BY strftime('%w', StartTime)";
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                String day = rs.getString("Day");
-                double timeInHours = rs.getInt("Duration") /60.0;
-                weeklySeries.getData().add(new XYChart.Data<>(day, timeInHours));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        updateGraph(weeklySeries, false);
+        updateGraph(reportDAO.getWeeklyReport(), false);
         detailedActivityTable.setVisible(false);
     }
 
     @FXML
     private void showDetailedActivityClick() {
-        // Retrieve and display detailed activity data
-        detailedActivityTable.setItems(getDetailedActivityData(isWeeklyReport));
+       if(isWeeklyReport) {
+           detailedActivityTable.setItems(reportDAO.getWeeklyReportDetail());
+       } else {
+           detailedActivityTable.setItems(reportDAO.getDailyReportDetail());
+       }
+
         detailedActivityTable.setVisible(true);
     }
 
@@ -170,53 +91,6 @@ public class ReportsController {
     private double calculateAverageMinutes(XYChart.Series<String, Number> series) {
         double totalMinutes = calculateTotalMinutes(series);
         return totalMinutes / series.getData().size();
-    }
-
-    private ObservableList<Activity> getDetailedActivityData(boolean isWeeklyReport) {
-        ObservableList<Activity> data = FXCollections.observableArrayList();
-
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:Wellness_Tracker.db");
-             Statement stmt = conn.createStatement()) {
-
-            String query;
-            if(isWeeklyReport) {
-                query = "SELECT " +
-                        "Application, " +
-                        "SUM((strftime('%s', EndTime) - strftime('%s', StartTime)) / 3600) || ' hours ' || SUM(((strftime('%s', EndTime) - strftime('%s', StartTime)) % 3600) / 60) || ' minutes' AS TimeSpent " +
-                        "FROM " +
-                        "Usage " +
-                        "WHERE " +
-                        "UserId = 1 " +
-                        "AND DATE(StartTime) BETWEEN DATE('now', '-7 days') AND DATE('now')  " +
-                        "GROUP BY " +
-                        "Application";
-            } else {
-                query = "SELECT " +
-                        "Application, " +
-                        "SUM((strftime('%s', EndTime) - strftime('%s', StartTime)) / 3600) || ' hours ' || SUM(((strftime('%s', EndTime) - strftime('%s', StartTime)) % 3600) / 60) || ' minutes' AS TimeSpent " +
-                        "FROM " +
-                        "Usage " +
-                        "WHERE " +
-                        "UserId = 1 " +
-                        "AND DATE(StartTime) = DATE('now') " +
-                        "GROUP BY " +
-                        "Application";
-            }
-
-            ResultSet rs = stmt.executeQuery(query);
-
-            // Populate the table data with the retrieved data
-            while (rs.next()) {
-                String application = rs.getString("Application");
-                String timeSpent = rs.getString("TimeSpent");
-                data.add(new Activity(application, timeSpent));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return data;
     }
 
     public static class Activity {
--- src/main/java/dao/ReportDAO.java
+++ src/main/java/dao/ReportDAO.java
@@ -1,98 +1,167 @@
 package dao;
+import java.sql.Connection;
+
+import com.example.wellbeing_project.ReportsController;
+import com.example.wellbeing_project.universal.AppUser;
+import com.example.wellbeing_project.universal.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.XYChart;
+
+import java.sql.*;
 
 public class ReportDAO {
 
-    public ReportDAO() {
+    private Connection connection;
 
+    public ReportDAO() {
+        connection = DBConnection.getInstance();
     }
 
-    public void getDailyReport() {
-
+    public XYChart.Series<String, Number> getDailyReport() {
+        XYChart.Series<String, Number> dailySeries = new XYChart.Series<>();
         String query = "SELECT " +
                 "CASE " +
-                "WHEN strftime('%H', StartTime) BETWEEN '00' AND '00' THEN '12am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '01' AND '01' THEN '1am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '02' AND '02' THEN '2am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '03' AND '03' THEN '3am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '04' AND '04' THEN '4am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '05' AND '05' THEN '5am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '06' AND '06' THEN '6am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '07' AND '07' THEN '7am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '08' AND '08' THEN '8am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '09' AND '09' THEN '9am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '10' AND '10' THEN '10am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '11' AND '11' THEN '11am' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '12' AND '12' THEN '12pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '13' AND '13' THEN '1pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '14' AND '14' THEN '2pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '15' AND '15' THEN '3pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '16' AND '16' THEN '4pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '17' AND '17' THEN '5pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '18' AND '18' THEN '6pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '19' AND '19' THEN '7pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '20' AND '20' THEN '8pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '21' AND '21' THEN '9pm' " +
-                "WHEN strftime('%H', StartTime) BETWEEN '22' AND '22' THEN '10pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '00' AND '00' THEN '12am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '01' AND '01' THEN '1am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '02' AND '02' THEN '2am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '03' AND '03' THEN '3am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '04' AND '04' THEN '4am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '05' AND '05' THEN '5am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '06' AND '06' THEN '6am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '07' AND '07' THEN '7am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '08' AND '08' THEN '8am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '09' AND '09' THEN '9am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '10' AND '10' THEN '10am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '11' AND '11' THEN '11am' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '12' AND '12' THEN '12pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '13' AND '13' THEN '1pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '14' AND '14' THEN '2pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '15' AND '15' THEN '3pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '16' AND '16' THEN '4pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '17' AND '17' THEN '5pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '18' AND '18' THEN '6pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '19' AND '19' THEN '7pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '20' AND '20' THEN '8pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '21' AND '21' THEN '9pm' " +
+                "WHEN strftime('%H', StartTime / 1000, 'unixepoch') BETWEEN '22' AND '22' THEN '10pm' " +
                 "ELSE '11pm' " +
                 "END AS Hour, " +
-                "SUM(strftime('%s', EndTime) - strftime('%s', StartTime)) / 60 AS Duration " +
+                "SUM((EndTime / 1000) - (StartTime / 1000)) / 60 AS Duration " +
                 "FROM " +
                 "Usage " +
                 "WHERE " +
                 "UserId = ? " +
-                "AND DATE(StartTime) = DATE('now') " +
+                "AND DATE(StartTime / 1000, 'unixepoch') = DATE('now') " +
                 "GROUP BY " +
                 "Hour " +
                 "ORDER BY " +
-                "strftime('%H', StartTime)";
+                "strftime('%H', StartTime / 1000, 'unixepoch')";
 
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, AppUser.getUserId());
+             ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String hour = resultSet.getString("Hour");
+                double timeInHours = resultSet.getInt("Duration") / 60.0;
+                dailySeries.getData().add(new XYChart.Data<>(hour, timeInHours));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dailySeries;
     }
 
-    public void getDailyReportDetail() {
+    public ObservableList<ReportsController.Activity> getDailyReportDetail() {
+        ObservableList<ReportsController.Activity> data = FXCollections.observableArrayList();
         String query = "SELECT " +
                 "Application, " +
-                "SUM((strftime('%s', EndTime) - strftime('%s', StartTime)) / 3600) || ' hours ' || SUM(((strftime('%s', EndTime) - strftime('%s', StartTime)) % 3600) / 60) || ' minutes' AS TimeSpent " +
+                "SUM((EndTime / 1000 - StartTime / 1000) / 3600) || ' hours ' || SUM(((EndTime / 1000 - StartTime / 1000) % 3600) / 60) || ' minutes' AS TimeSpent " +
                 "FROM " +
                 "Usage " +
                 "WHERE " +
                 "UserId = ? " +
-                "AND DATE(StartTime) = DATE('now') " +
+                "AND DATE(StartTime / 1000, 'unixepoch')  = DATE('now') " +
                 "GROUP BY " +
                 "Application";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, AppUser.getUserId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String application = resultSet.getString("Application");
+                String timeSpent = resultSet.getString("TimeSpent");
+                data.add(new ReportsController.Activity(application, timeSpent));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+
 
     }
 
-    public void getWeeklyReport() {
+    public XYChart.Series<String, Number> getWeeklyReport() {
+        XYChart.Series<String, Number> weeklySeries = new XYChart.Series<>();
         String query = "SELECT " +
                 "CASE " +
-                "WHEN strftime('%w', StartTime) = '0' THEN 'Sunday' " +
-                "WHEN strftime('%w', StartTime) = '1' THEN 'Monday' " +
-                "WHEN strftime('%w', StartTime) = '2' THEN 'Tuesday' " +
-                "WHEN strftime('%w', StartTime) = '3' THEN 'Wednesday' " +
-                "WHEN strftime('%w', StartTime) = '4' THEN 'Thursday' " +
-                "WHEN strftime('%w', StartTime) = '5' THEN 'Friday' " +
-                "WHEN strftime('%w', StartTime) = '6' THEN 'Saturday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '0' THEN 'Sunday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '1' THEN 'Monday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '2' THEN 'Tuesday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '3' THEN 'Wednesday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '4' THEN 'Thursday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '5' THEN 'Friday' " +
+                "WHEN strftime('%w',  StartTime / 1000, 'unixepoch') = '6' THEN 'Saturday' " +
                 "END AS Day, " +
-                "SUM(strftime('%s', EndTime) - strftime('%s', StartTime)) / 60 AS Duration " +
+                "SUM((EndTime / 1000) - (StartTime / 1000)) / 60 AS Duration " +
                 "FROM Usage " +
                 "WHERE UserId = ? " +
-                "AND DATE(StartTime) BETWEEN DATE('now', '-7 days') AND DATE('now') " +
-                "GROUP BY strftime('%w', StartTime)";
+                "AND DATE(StartTime / 1000, 'unixepoch') BETWEEN DATE('now', '-7 days') AND DATE('now') " +
+                "GROUP BY strftime('%w', StartTime / 1000, 'unixepoch')";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, AppUser.getUserId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String day = resultSet.getString("Day");
+                double timeInHours = resultSet.getInt("Duration") /60.0;
+                weeklySeries.getData().add(new XYChart.Data<>(day, timeInHours));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return weeklySeries;
 
     }
 
-    public void getWeeklyReportDetail() {
-
+    public ObservableList<ReportsController.Activity> getWeeklyReportDetail() {
+        ObservableList<ReportsController.Activity> data = FXCollections.observableArrayList();
         String query = "SELECT " +
                 "Application, " +
-                "SUM((strftime('%s', EndTime) - strftime('%s', StartTime)) / 3600) || ' hours ' || SUM(((strftime('%s', EndTime) - strftime('%s', StartTime)) % 3600) / 60) || ' minutes' AS TimeSpent " +
+                "SUM((EndTime / 1000 - StartTime / 1000) / 3600) || ' hours ' || SUM(((EndTime / 1000 - StartTime / 1000) % 3600) / 60) || ' minutes' AS TimeSpent " +
                 "FROM " +
                 "Usage " +
                 "WHERE " +
                 "UserId = ? " +
-                "AND DATE(StartTime) BETWEEN DATE('now', '-7 days') AND DATE('now')  " +
+                "AND DATE(StartTime / 1000, 'unixepoch')  BETWEEN DATE('now', '-7 days') AND DATE('now')  " +
                 "GROUP BY " +
                 "Application";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, AppUser.getUserId());
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String application = resultSet.getString("Application");
+                String timeSpent = resultSet.getString("TimeSpent");
+                data.add(new ReportsController.Activity(application, timeSpent));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
 
     }
 }
